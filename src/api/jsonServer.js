57d3741aeb4c7c3ed0dee866b975e4beeba7fc5a
Yanay Zabary <yanay.tsabary@gmail.com>
2020-01-01 19:04:18 +0200

    initial commit

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL:'http://88ec1f0f.ngrok.io'
+});
---
+++ src/components/BlogPostForm.js
@@ -0,0 +1,61 @@
+import React, { useState, useContext } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const BlogPostForm = ({ onSubmit, initialValues }) => {
+
+    const [title, setTitle] = useState(initialValues.title);
+    const [content, setContent] = useState(initialValues.content);
+
+    return <View>
+        <Text
+            style={styles.label}>
+            Enter title:
+            </Text>
+        <TextInput
+            style={styles.input}
+            value={title}
+            onChangeText={(title) => setTitle(title)}
+        />
+
+        <Text
+            style={styles.label}>
+            Enter Content:
+            </Text>
+        <TextInput
+            style={styles.input}
+            value={content}
+            onChangeText={(content) => setContent(content)}
+        />
+
+        <Button
+            title="Save blog post"
+            onPress={() => { onSubmit(title, content) }} />
+    </View>
+};
+
+BlogPostForm.defaultProps = {
+    initialValues: {
+        title: '',
+        content: ''
+    }
+};
+
+const styles = StyleSheet.create({
+    input: {
+        fontSize: 18,
+        borderColor: 'black',
+        borderWidth: 1,
+        marginBottom: 15,
+        padding: 5,
+        margin: 5
+
+    },
+    label: {
+        fontSize: 20,
+        marginBottom: 5,
+        marginLeft: 5,
+
+    }
+});
+
+export default BlogPostForm;
